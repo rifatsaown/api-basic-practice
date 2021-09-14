dfdfd0cf9eb03000543dbc6c64d72c0a6c188f40
rifatsaown <rifatsaown0@gmail.com>
2021-09-14 23:03:36 +0600

    end

--- random-user.js
+++ random-user.js
@@ -1,5 +1,17 @@
 const userData = () => {
-    fetch("https://randomuser.me/api/?results=5")
-    .then(response => response.json())
-    .then(data => data)
+    const userInput = document.getElementById('userInput');
+    const userNumber = parseInt(userInput.value);
+    fetch(`https://randomuser.me/api/?results=${userNumber}`)
+        .then(response => response.json())
+        .then(data => showData(data))
+}
+
+const showData = data => {
+    const users = data.results;
+    const userContainer = document.getElementById('userContainer');
+    for (const user of users) {
+        const div = document.createElement('div');
+        div.innerHTML = `<p> Name : ${user.name.title} ${user.name.first} ${user.name.last} </p> <p> Email: ${user.email} </p> <p> Phone : ${user.phone} </p> <p> Gender : ${user.gender} </p> <p> Age : ${user.dob.age}</p> <p>Date of Birth : ${user.dob.date}</p><br> <br>`;
+        userContainer.appendChild(div);
+    }
 }
